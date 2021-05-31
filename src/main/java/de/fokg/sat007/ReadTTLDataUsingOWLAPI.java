0a13a825a2a057c93326e2a80514404b40fb2c38
soumy <soumy@host.docker.internal>
2021-05-31 21:09:09 +0200

    Changed the Logic for positive and negative files.

--- src/main/java/de/fokg/sat007/ReadTTLDataUsingOWLAPI.java
+++ src/main/java/de/fokg/sat007/ReadTTLDataUsingOWLAPI.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Set;
+import java.util.TreeSet;
 
 import org.semanticweb.owlapi.apibinding.OWLManager;
 import org.semanticweb.owlapi.model.OWLAnnotation;
@@ -22,7 +23,7 @@ import org.semanticweb.owlapi.util.OWLOntologyWalker;
 import org.semanticweb.owlapi.util.OWLOntologyWalkerVisitor;
 
 /**
- * @author Atul
+ * @author Atul, Sowmya
  *
  */
 public class ReadTTLDataUsingOWLAPI {
@@ -34,7 +35,7 @@ public class ReadTTLDataUsingOWLAPI {
 		OWLDataFactory df = OWLManager.getOWLDataFactory();
 		
 		// Get data in Hash Map
-		HashMap<String, HashMap<String, ArrayList<String>>> mapOfData =	readData(o, df);
+		HashMap<String, HashMap<String, TreeSet<String>>> mapOfData =	readData(o, df);
 		System.out.println("Size of Map : " + mapOfData.size());
 
 		//Create files as per data in HashMap
@@ -46,7 +47,7 @@ public class ReadTTLDataUsingOWLAPI {
 	/**
 	 * @param mapOfData
 	 */
-	private static void createNewFiles(HashMap<String, HashMap<String, ArrayList<String>>> mapOfData) {
+	private static void createNewFiles(HashMap<String, HashMap<String, TreeSet<String>>> mapOfData) {
 		Set<String> lpKeys = mapOfData.keySet();//Get Learning Problem Keys
 		for(String lpKey: lpKeys ) {
 			String fileName = lpKey.replaceAll("https://lpbenchgen.org/resource/", "");
@@ -66,17 +67,17 @@ public class ReadTTLDataUsingOWLAPI {
 			try {
 				//Write data for positive samples
 				FileOutputStream fileOut = new FileOutputStream(file);
-				ArrayList<String> listIncludeResources = mapOfData.get(lpKey).get("<https://lpbenchgen.org/property/includesResource>");
+				TreeSet<String> listIncludeResources = mapOfData.get(lpKey).get("<https://lpbenchgen.org/property/includesResource>");
 				for(String pos: listIncludeResources) {
-					pos= pos + "\n";
+					pos= pos.replaceAll("http://dl-learner.org/carcinogenesis#", "")  + "\n";
 					fileOut.write(pos.getBytes());
 				}
 				fileOut.close();
 
 				FileOutputStream fileOut1 = new FileOutputStream(file1);
-				ArrayList<String> mapExcludeResources = mapOfData.get(lpKey).get("<https://lpbenchgen.org/property/excludesResource>");
+				TreeSet<String> mapExcludeResources = mapOfData.get(lpKey).get("<https://lpbenchgen.org/property/excludesResource>");
 				for(String neg: mapExcludeResources) {
-					neg= neg + "\n";
+					neg= neg.replaceAll("http://dl-learner.org/carcinogenesis#", "") + "\n";
 					fileOut1.write(neg.getBytes());
 				}
 				fileOut1.close();
@@ -96,7 +97,7 @@ public class ReadTTLDataUsingOWLAPI {
 	/**
 	 * @param mapOfData
 	 */
-	private static void deleteOldFiles(HashMap<String, HashMap<String, ArrayList<String>>> mapOfData) {
+	private static void deleteOldFiles(HashMap<String, HashMap<String, TreeSet<String>>> mapOfData) {
 		Set<String> lpKeys = mapOfData.keySet();//Get Learning Problem Keys
 		for(String lpKey: lpKeys ) {
 			String fileName = lpKey.replaceAll("https://lpbenchgen.org/resource/", "");
@@ -112,10 +113,10 @@ public class ReadTTLDataUsingOWLAPI {
 	 * @param df
 	 * @return
 	 */
-	private static HashMap<String, HashMap<String, ArrayList<String>>> readData(OWLOntology o, OWLDataFactory df) {
+	private static HashMap<String, HashMap<String, TreeSet<String>>> readData(OWLOntology o, OWLDataFactory df) {
 		
 		// Initialize Map
-		HashMap<String, HashMap<String, ArrayList<String>>> mapOfData = new HashMap<>();
+		final HashMap<String, HashMap<String, TreeSet<String>>> mapOfData = new HashMap<>();
 		
 		// Code for class
 		/*OWLClass clas = null;
@@ -137,9 +138,9 @@ public class ReadTTLDataUsingOWLAPI {
 				
 				// Logic to store data in map
 				if (mapOfData.get(subject.toString()) == null) { // If learning problem not present in the map then create a entry
-					HashMap<String, ArrayList<String>> resMap = new HashMap<>();
-					ArrayList<String> tempInclude = new ArrayList<>();
-					ArrayList<String> tempExclude = new ArrayList<>();
+					HashMap<String, TreeSet<String>> resMap = new HashMap<>();
+					TreeSet<String> tempInclude = new TreeSet<>();
+					TreeSet<String> tempExclude = new TreeSet<>();
 
 					if (annotation.toString().contains("includesResource")) { // If include resource then add the value in Include list
 						tempInclude.add(value.toString());
@@ -156,11 +157,11 @@ public class ReadTTLDataUsingOWLAPI {
 				} else {// Entry already present in the map
 					
 					if (annotation.toString().contains("includesResource")) {// If include resource then update the value in Include list
-						ArrayList<String> tempInclude = mapOfData.get(subject.toString()).get("<https://lpbenchgen.org/property/includesResource>");
+						TreeSet<String> tempInclude = mapOfData.get(subject.toString()).get("<https://lpbenchgen.org/property/includesResource>");
 						tempInclude.add(value.toString());
 						
 					} else {// If exclude resource then update the value in Exclude list
-						ArrayList<String> tempExclude = mapOfData.get(subject.toString()).get("<https://lpbenchgen.org/property/excludesResource>");
+						TreeSet<String> tempExclude = mapOfData.get(subject.toString()).get("<https://lpbenchgen.org/property/excludesResource>");
 						tempExclude.add(value.toString());
 					}
 					
